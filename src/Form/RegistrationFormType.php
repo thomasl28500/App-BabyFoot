b2fb3a38b70babfb73b5890a5eec8ced701d0cab
Thomas <112702448+thomasl28500@users.noreply.github.com>
2024-04-24 11:23:08 +0200

    add template

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('nickName')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
